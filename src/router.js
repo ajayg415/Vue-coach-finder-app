f0fa6b7c7094f9b9fe95ca9ea9ffa88e72a5f080
ajayg415 <ajay.g415@gmail.com>
2021-10-30 23:57:51 +0530

    added header and basic routing

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import CoachDetail from './pages/coaches/CoachDetail.vue'
-import CoachList from './pages/coaches/CoachDetail.vue'
-import CoachRegistration from './pages/coaches/CoacheRegistration.vue'
+import CoachList from './pages/coaches/CoachesList.vue'
+import CoachRegistration from './pages/coaches/CoachRegistration.vue'
 import ContactCoach from './pages/requests/ContactCoach.vue'
 import RequestsReceived from './pages/requests/RequestsReceived.vue'
 import NotFound from './pages/NotFound.vue'
