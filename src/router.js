c79d899f0fcc10079aef0e7fd733e738c6ca11b1
ajayg415 <ajay.g415@gmail.com>
2021-10-30 23:37:14 +0530

    added router to main

--- src/router.js
+++ src/router.js
@@ -1,17 +1,25 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import CoachDetail from './pages/coaches/CoachDetail.vue'
+import CoachList from './pages/coaches/CoachDetail.vue'
+import CoachRegistration from './pages/coaches/CoacheRegistration.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestsReceived from './pages/requests/RequestsReceived.vue'
+import NotFound from './pages/NotFound.vue'
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', redirect: '/coaches' },
-        { path: '/coaches', component: null },
-        { path: '/coaches/:id', component: null, 
+        { path: '/coaches', component: CoachList},
+        { path: '/coaches/:id', component: CoachDetail, 
             children: [
-                { path: 'contact', component: null },
+                { path: 'contact', component: ContactCoach },
             ]
         },
-        { path: '/register', component: null },
-        { path: '/:notFound(.*)', component: null }
+        { path: '/register', component: CoachRegistration },
+        { path: '/requests', component: RequestsReceived },
+        { path: '/:notFound(.*)', component: NotFound }
     ]
 })
 
