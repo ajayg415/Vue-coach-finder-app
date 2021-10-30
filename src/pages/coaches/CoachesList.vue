5a9f77f94776221b1e14dfc865d66738ce64460e
ajayg415 <ajay.g415@gmail.com>
2021-10-31 01:05:07 +0530

    added CoachesItem component

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,16 +6,27 @@
       <router-link to="register">Register as Coach</router-link>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
-        {{ coach.firstName }}
-      </li>
+      <CoachItem
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :rate="coach.hourlyRate"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :areas="coach.areas"
+      />
     </ul>
     <h3 v-else>No Coaches Found</h3>
   </section>
 </template>
 
 <script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
+
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
     filteredCoaches() {
       return this.$store.getters["coaches/coaches"];
@@ -26,3 +37,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
