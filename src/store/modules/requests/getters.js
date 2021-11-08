7d285bcd43eec52c4503d1ec9f62d7dcdf20904b
ajayg415 <ajay.g415@gmail.com>
2021-11-08 09:21:20 +0530

    Added requests view page

--- src/store/modules/requests/getters.js
+++ src/store/modules/requests/getters.js
@@ -1,1 +1,18 @@
-export default {}
+// export default {
+//     requests(state) {
+//         return state.requests
+//     },
+//     hasReqests(state) {
+//         return state.requests.length > 0
+//     }
+// }
+
+export default {
+    requests(state, _getters, _rootState, rootGetters) {
+      const coachId = rootGetters.userId;
+      return state.requests.filter(req => req.coachId === coachId);
+    },
+    hasRequests(_state, getters) {
+      return getters.requests && getters.requests.length > 0;
+    }
+  };
